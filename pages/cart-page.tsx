8acf3e8766d1fe874dc4be118f28af18504358af
Geodor Ruales <Geodor.Ruales@telus.com>
2022-06-17 00:14:29 +0800

    style: add unicorn logo to cart page

--- pages/cart-page.tsx
+++ pages/cart-page.tsx
@@ -1,10 +1,12 @@
 import { CartTable } from '../components/cart'
 import { Typography, ButtonLink } from '@telus-uds/ds-allium'
 import styles from '../styles/CartPage.module.css'
+import { UnicornLogo } from '../components'
 
 export default function CartPage() {
   return (
     <>
+      <UnicornLogo />
       <div className={styles.cartTitleWrapper}>
         <Typography variant={{ size: 'h1' }}>Cart</Typography>
       </div>
